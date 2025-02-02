35be3c36c7e2d8f989f341de59aea21d3b6ffd9d
SajjadDaneshmand <Mr.SajjadDaneshmand@gmail.com>
2025-02-02 08:47:34 +0330

    create serializer for diary model

---
+++ diaries_api/diary/serializers.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+from .models import Diary
+
+
+class DiarySerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Diary
+        fields = ('owner.username', 'title', 'description', 'created_at')
+        read_only_fields = ('created_at', )
