2fc7a07834cbfc48f1bcdb877ba515a750af4da4
SajjadDaneshmand <Mr.SajjadDaneshmand@gmail.com>
2025-02-02 08:45:53 +0330

    create model for diary

--- diaries_api/diary/models.py
+++ diaries_api/diary/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Diary(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_public = models.BooleanField(default=True)
