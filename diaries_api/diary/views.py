e94f87f63e7cdb514b078725201cc779388a436b
SajjadDaneshmand <Mr.SajjadDaneshmand@gmail.com>
2025-02-02 10:53:52 +0330

    define views for diary app

--- diaries_api/diary/views.py
+++ diaries_api/diary/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+# rest_framework
+from rest_framework import viewsets, mixins
+from rest_framework.response import Response
 
-# Create your views here.
+# internal
+from diary.models import Diary
+from diary.serializers import DiarySerializer
+
+
+class DiaryViewSet(
+            mixins.CreateModelMixin,
+            mixins.RetrieveModelMixin,
+            mixins.DestroyModelMixin,
+            mixins.ListModelMixin,
+            viewsets.GenericViewSet
+        ):
+
+    queryset = Diary.objects.all()
+    serializer_class = DiarySerializer
+    # permission_classes = []  # TODO: Specify permissions
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
