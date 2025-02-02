d3243c03e5609ea1db56d2c792877644252537f0
SajjadDaneshmand <Mr.SajjadDaneshmand@gmail.com>
2025-02-02 10:50:42 +0330

    build routes for diary app

--- diaries_api/diary/urls.py
+++ diaries_api/diary/urls.py
@@ -1,6 +1,18 @@
-from django.urls import path
+# django
+from django.urls import path, include
+
+# rest_framework
+from rest_framework.routers import DefaultRouter
+
+# internal
 from diary import views
 
 
+# create a router and register views
+router = DefaultRouter()
+router.register(r'', views.DiaryViewSet)
+
+
 urlpatterns = [
+    path('', include(router.urls))
 ]
