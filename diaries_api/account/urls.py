80ce3c008ce3f54af864e4a9af06e72c15ba7093
SajjadDaneshmand <Mr.SajjadDaneshmand@gmail.com>
2025-02-02 10:51:29 +0330

    create new file for account routes - also define routes

---
+++ diaries_api/account/urls.py
@@ -0,0 +1,16 @@
+# django
+from django.urls import path, include
+
+# rest_framework
+from rest_framework.routers import DefaultRouter
+
+# internal
+from account import views
+
+
+router = DefaultRouter()
+router.register(r'', views.UserViewSet)
+
+urlpatterns = [
+    path('', include(router.urls))
+]
