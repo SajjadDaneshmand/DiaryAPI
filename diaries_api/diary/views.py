f1b23c3f5ddf5ce2b3b0fb9e9f45ddb51de4f757
SajjadDaneshmand <Mr.SajjadDaneshmand@gmail.com>
2025-02-04 13:23:55 +0330

    try private diaries not show to anyone except owner - but still not successfull

--- diaries_api/diary/views.py
+++ diaries_api/diary/views.py
@@ -1,10 +1,10 @@
 # rest_framework
-from rest_framework import viewsets, mixins
-from rest_framework.response import Response
+from rest_framework import viewsets, mixins, permissions
 
 # internal
 from diary.models import Diary
 from diary.serializers import DiarySerializer
+from diary.permissions import IsDiaryAccessible
 
 
 class DiaryViewSet(
@@ -17,7 +17,12 @@ class DiaryViewSet(
 
     queryset = Diary.objects.all()
     serializer_class = DiarySerializer
-    # permission_classes = []  # TODO: Specify permissions
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                          IsDiaryAccessible, )
+
+    def list(self, request, *args, **kwargs):
+        self.queryset = self.queryset.filter(is_public=True)
+        super().list(request, *args, **kwargs)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
