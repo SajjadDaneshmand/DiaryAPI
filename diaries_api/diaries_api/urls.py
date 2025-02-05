da8f45ba3112a7bb30ed73db0ff62cb69154a997
SajjadDaneshmand <Mr.SajjadDaneshmand@gmail.com>
2025-02-05 18:24:21 +0330

    add rest_framework built in route for users in urls file

--- diaries_api/diaries_api/urls.py
+++ diaries_api/diaries_api/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('diaries/', include('diary.urls')),
     path('users/', include('account.urls')),
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
 ]
